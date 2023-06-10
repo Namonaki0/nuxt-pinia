15e4b6947f70e80c688b404dfe2ddead3ae28963
Andre <andreferreira@live.co.uk>
2023-06-10 11:46:23 +0100

    json-server data fetch and rendering

--- pages/index.vue
+++ pages/index.vue
@@ -2,4 +2,17 @@
     <div>
         <h2 class="text-white text-2xl text-center my-8">All products</h2>
     </div>
+    <div class="gird grid-cols-2 gap-7">
+        <div 
+            v-for="product in products"
+            :key="product.id"    
+        >
+            {{ product.title }}
+        </div>
+    </div>
 </template>
+
+<script setup>
+    // fetch products
+    const { data: products } = await useFetch('http://localhost:3000/products');
+</script>
