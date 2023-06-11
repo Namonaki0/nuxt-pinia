97b43ae8d3dbd7aea13557912e7b0ad328ca69b9
Andre <andreferreira@live.co.uk>
2023-06-11 23:01:42 +0100

    ProductCard component and prop rendering

---
+++ components/ProductCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+    const props = defineProps({
+        product: {
+            type: Object,
+            default: () => {}
+        }
+    })
+</script>
+<template>
+    <div class="card flex items-center gap-8">
+        <img :src="product.img" alt="product.title">
+        <div>
+            <p class="text-2xl text-secondary">{{ product.title }}</p>
+            <p class="text-xl text-gray-50">{{ product.description }}</p>
+            <p class="text-l text-secondary my-3">{{ product.price }} Silver coins</p>
+            <button class="btn">
+                <span>Add to basket</span>
+            </button>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
     <div>
         <h2 class="text-white text-2xl text-center my-8">All products</h2>
     </div>
-    <div class="gird grid-cols-2 gap-7">
+    <div class="grid grid-cols-2 gap-7">
         <div 
             v-for="product in products"
             :key="product.id"    
         >
-            {{ product.title }}
+            <ProductCard 
+                :product="product"
+            />
         </div>
     </div>
 </template>
 
 <script setup>
     // fetch products
+    import ProductCard from '@/components/ProductCard.vue'
     const { data: products } = await useFetch('http://localhost:3000/products');
 </script>
